76bfeabe2031168d06078aac0828ccc9f518ef42
Yamanuri Praneeth <161028719+Praneeth5158@users.noreply.github.com>
2025-12-02 20:55:32 +0530

    Add files via upload

---
+++ personscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Personscreen extends StatefulWidget {
+  const Personscreen({super.key});
+
+  @override
+  State<Personscreen> createState() => _PersonscreenState();
+}
+
+class _PersonscreenState extends State<Personscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+    );
+  }
+}
